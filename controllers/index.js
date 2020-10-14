e6233d34268fa7aa1888d878aad2f6dad8d36a8a
Prady2006 <pradyumnsharma7@gmail.com>
2020-10-15 00:29:30 +0530

    successfully sending coordinates

--- controllers/index.js
+++ controllers/index.js
@@ -15,21 +15,33 @@ module.exports.admin= function(req, res){
 
 module.exports.data =  (req, res) =>{
     console.log('inside data controller :',req.body);
-    const spawn = require('child_process').spawn;
-    const PyProcess = spawn('python',[path.join(__dirname, '../scripts/pgeocode.py'),req.body.postalCode,req.body.country]);
-    PyProcess.stdout.on('data',(data)=>{
-        res.send(data.toString());
-    });
-    PyProcess.on('exit', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
+    // const spawn = require('child_process').spawn;
+    // const PyProcess = spawn('python',[path.join(__dirname, '../scripts/pgeocode.py'),req.body.postalCode]);
+    // PyProcess.stdout.on('data',(data)=>{
+    //     console.log(' data :',data );
+    //     return res.send(data);
+    // })
+    // PyProcess.on('exit', (code) => {
+    //     console.log(`child process exited with code ${code}`);
+    // });
+    const CMD = 'python .\\pgeocode.py ' + req.body.postalCode
+    const exect = require('child_process').exec(CMD,{cwd:path.join(__dirname,'../scripts')} ,(error, stdout,stderr)=> {
+        // console.log('cwd : ',cwd);
+        if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        return res.status(200).json(stdout);
+    })
 }
 
 module.exports.fetchDataFromPythonScript = async (req,res) =>{
     console.log('body : ',req.body);
     return res.status(200).json({
         data: {
-            message: "long and latt "
+            message: req.body
         }
     });
 }
--- scripts/pgeocode.py
+++ scripts/pgeocode.py
@@ -1,16 +1,23 @@
-import pgeocode
+from pyzipcode import ZipCodeDatabase
 import sys
 import requests 
 
 URL = "http://127.0.0.1:8000/fetchdata"
 def findCoordinates():
-    print('running the python script')
-    postalCode = sys.argv[1]
-    country = sys.argv[2]
-    nomi = pgeocode.Nominatim(country)
-    obj = nomi.to_json();
-    response = requests.post(URL,data=obj);
-    print(response.text);
+    # print(sys.argv[1]);
+    zcdb = ZipCodeDatabase()
+    code = int(sys.argv[1])
+    # print(sys.argv[1],type(sys.argv[1]));
+    nomi = zcdb[code]
+    obj = {
+        "latitude": nomi.latitude,
+        "longitude": nomi.longitude 
+    }
+    # obj = {}
+    # res = requests.post(URL,data=obj);
+    print(obj);
+    # print(obj);
+  
 
-findCoordinates()
-
+if __name__ == "__main__":
+    findCoordinates()
